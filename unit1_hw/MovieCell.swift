debee8ff7e1dec8c8756a54f89d76b2b9c315af2
Sam Raykhman <sam.raykhman@gmail.com>
2022-02-15 23:07:28 -0500

    Add files via upload
    manual upload due to Console errors

---
+++ unit1_hw/MovieCell.swift
@@ -0,0 +1,27 @@
+//
+//  MovieCell.swift
+//  unit1_hw
+//
+//  Created by Sam Raykhman on 2/15/22.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var posterView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
